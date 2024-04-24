416a63d8881a09e03e91d4cb8482ece4bbeb4532
Tata <tabata.rmsantos@gmail.com>
2024-04-24 11:42:44 -0300

    24-04

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const logo = document.getElementById("logo");
 const overlay = document.getElementById("overlay");
 
-logo.addEventListener("click", function() {
-    if (overlay.style.zIndex === "-1") {
+logo.addEventListener("click", function(event) {
         overlay.style.zIndex = "1";
-    } else {
-        overlay.style.zIndex = "-1";
-    }
+        event.stopPropagation();
 });
+
+overlay.addEventListener("click", function(event){
+    event.stopPropagation();
+})
  
+document.addEventListener("click", function() {
+    overlay.style.zIndex = "-1";
+});
