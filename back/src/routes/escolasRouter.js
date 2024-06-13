c18bf2d3189fc4e9ac010f0e14c649c7c2d746ea
TABATA RAMOS DOS SANTOS <60045064008@senacrs.edu.br>
2024-06-13 14:41:01 -0300

    13-06

--- back/src/routes/escolasRouter.js
+++ back/src/routes/escolasRouter.js
@@ -3,7 +3,7 @@ const router = Router();
 
 const { storeEscolas, retornaEscolas } = require('../controllers/escolasController');
 
-router.post('/store/escola', storeEscolas);
+router.post('/store/escolas', storeEscolas);
 router.get('/get/escolas/:cidade', retornaEscolas);
 
 module.exports = router;
