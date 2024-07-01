7f2a061c49416a897650f62d588e213369e9d356
tata <tabata.rmsantos@gmail.com>
2024-07-01 09:27:58 -0300

    01-07

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ const selectCidade = document.getElementById('cidade');
 button.addEventListener('click', async () =>{
     const cidadeSelecionada = selectCidade.value;
 
-    const response = await fetch(`http://localhost:3003/api/get/escolas?:cidade=${cidadeSelecionada}`);
+    const response = await fetch(`http://localhost:3003/api/get/escolas/${cidadeSelecionada}`);
     const result = await response.json();
-
-    if(result.sucess) {
-        const cursosDiv = document.querySelectorAll('.cursos');
+    console.log(result);
+    if(result.success) {
+        const cursosDiv = document.querySelectorAll('.cursos')[0];
         cursosDiv.innerHTML = '';
 
         result.data.forEach(escolas => {
-            const nome = document.createElemente('p');
+            const nome = document.createElement('p');
             nome.textContent = escolas.nome;
 
             cursosDiv.appendChild(nome);
--- login.js
+++ login.js
@@ -19,3 +19,56 @@ but_oportunizado.addEventListener("click", function() {
         cadastro_oportunizado.style.zIndex = "-1";
     }
 });
+
+ let enviarOportuniza = document.getElementById("enviarOportuniza");
+ enviarOportuniza.onclick = async function() {
+
+    let nomeOportuniza = document.getElementById("nomeOportuniza").value;
+    let data_nascOportuniza = document.getElementById("data_nascOportuniza").value;
+    let cnpjOportuniza = document.getElementById("cnpjOportuniza").value;
+    let emailOportuniza = document.getElementById("emailOportuniza").value;
+    let celOportuniza = document.getElementById("celOportuniza").value;
+    let experiênciasOportuniza = document.getElementById("experiênciasOportuniza").value;
+    let senhaOportuniza = document.getElementById("senhaOportuniza").value;
+    let dançaOportuniza = document.getElementById("dançaOportuniza").value;
+    let teatroOportuniza = document.getElementById("teatroOportuniza").value;
+    let musicaOportuniza = document.getElementById("musicaOportuniza").value;
+    let docOportuniza = document.getElementById("docOportuniza").value;
+    let data = {nomeOportuniza, data_nascOportuniza, cnpjOportuniza, emailOportuniza, celOportuniza, experiênciasOportuniza, senhaOportuniza, dançaOportuniza, teatroOportuniza, musicaOportuniza, docOportuniza};
+
+    const response = await fetch('http://localhost:3003/api/store/oportuniza', {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(data)
+    });
+
+    let content = await response.json();
+    console.log(err.response.data)
+
+    if(content.sucess) {
+        alert("Sucesso")
+    } else {
+        alert("Não");
+    }
+}
+
+let enviarOportunizado = document.getElementById("enviarOportunizado");
+enviarOportunizado.onclick = async function() {
+
+    let form = document.getElementById("form");
+    let dadosForm = new FormData(form);
+
+    const response = await fetch('http://localhost:3003/api/store/oportuniza', {
+       method: "POST",
+       body: dadosForm
+   });
+
+   let content = await response.json();
+   console.log(err.response.data)
+
+   if(content.sucess) {
+       alert("Sucesso")
+   } else {
+       alert("Não");
+   }
+}
