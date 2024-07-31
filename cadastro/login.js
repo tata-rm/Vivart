35de9ac613b17901e6132892da797fe8875754aa
Tata <tabata.rmsantos@gmail.com>
2024-07-31 11:58:59 -0300

    31-07

--- cadastro/login.js
+++ cadastro/login.js
@@ -1,3 +1,20 @@
+const opcoes_oportunizado = document.getElementById("opcoes_oportunizado");
+const opcoes_oportuniza = document.getElementById("opcoes_oportuniza");
+const login_oportunizado = document.getElementById("login_oportunizado");
+const login_oportuniza = document.getElementById("login_oportuniza");
+
+opcoes_oportunizado.addEventListener("click", function(event) {
+    login_oportunizado.style.zIndex = "2";
+    event.stopPropagation();
+});
+
+opcoes_oportuniza.addEventListener("click", function(event){
+    login_oportuniza.style.zIndex = "2";
+    event.stopPropagation();
+})
+
+/*---------------------------------------------------------------*/
+
 async function getOportuniza() {
 
     const data = {cnpj: '12345678901234', senha: 'tata'}
