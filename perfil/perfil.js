013f9a63367aaf80a8540a24c8c14bab1efff0f1
Tata <tabata.rmsantos@gmail.com>
2024-11-25 01:01:09 -0300

    24-11

--- perfil/perfil.js
+++ perfil/perfil.js
@@ -56,11 +56,29 @@ let enviarPost = document.getElementById('enviarPost');
 enviarPost.onclick = async function(event) {
     event.preventDefault();
 
+    const storedAccount = localStorage.getItem('@account_logged');
+  
+    if (storedAccount) {
+        const account = JSON.parse(storedAccount);
+        document.querySelector("#profile-pic").setAttribute("src", `../src/uploads/${account.profile_pic}`)
+        document.querySelector("#input-name").value = account.name;
+        document.querySelector("#input-date").value = account.cpf;
+    } else {
+        console.error('Nenhuma conta está logada.');
+    }
+
     let camposPost = document.getElementById('camposPost');
     let dadosPost = new FormData(camposPost);
     let textPost = document.getElementById("textPost");
+    let nomePost = account.name;
+    let cpfPost = account.cpf;
+    let perfilPost = account.profile_pic;
 
     dadosPost.append('textPost', textPost);
+    dadosPost.append('nomePost', nomePost);
+    dadosPost.append('cpfPost', cpfPost);
+    dadosPost.append('perfilPost', perfilPost);
+
 
     try {
         const response = await fetch('http://localhost:3005/api/store/post', {
@@ -81,14 +99,6 @@ enviarPost.onclick = async function(event) {
         alert("Houve um problema ao processar a publicação.");
     }
 };
-      //if (response.ok) {
-     //   document.getElementById("post-content").value = ""; // Limpar o campo
-       // loadPosts(); // Atualizar o feed com a nova postagem
-     // }
-   // } else {
-      //alert("Por favor, escreva algo antes de publicar.");
-    //}
-
   
 /*---------------------------------------------------*/
 
